0823371068e747e235d9878161363b26163be654
JRamos29 <malebolge@gmail.com>
2021-11-05 19:18:59 -0300

    Configured FirebaseAuthService in firebase/authService.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+import FirebaseAuthService from './firebase/authService';
 import './App.scss';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import firebase from './firebase/config';
 
 function App() {
+  const [user, setUse] = useState(null);
+
   return (
     <div className="App">
       <div className="title-row">
