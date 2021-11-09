35a737455d661d348f33eb8c5e3a4c76bd931839
JRamos29 <malebolge@gmail.com>
2021-11-09 15:37:10 -0300

    Implemented onUpdateRecipe firebase function.

--- functions/index.js
+++ functions/index.js
@@ -96,3 +96,42 @@ exports.onDeleteRecipe = functions.firestore
       }
     }
   });
+
+exports.onUpdateRecipe = functions.firestore
+  .document('recipes/{recipeId}')
+  .onUpdate(async (changes) => {
+    const oldRecipe = changes.before.data();
+    const newRecipe = changes.after.data();
+
+    let publishCount = 0;
+
+    if (!oldRecipe.isPublished && newRecipe.isPublished) {
+      publishCount += 1;
+    } else if (oldRecipe.isPublished && !newRecipe.isPublished) {
+      publishCount -= 1;
+    }
+
+    if (publishCount !== 0) {
+      const publishedCountDocRef = firestore
+        .collection('recipeCounts')
+        .doc('published');
+
+      const publishedCountDoc = await publishedCountDocRef.get();
+
+      if (publishedCountDoc.exists) {
+        publishedCountDocRef.update({
+          count: admin.firestore.FieldValue.increment(publishCount),
+        });
+      } else {
+        if (publishCount > 0) {
+          publishedCountDocRef.set({
+            count: publishCount,
+          });
+        } else {
+          publishedCountDocRef.set({
+            count: 0,
+          });
+        }
+      }
+    }
+  });
