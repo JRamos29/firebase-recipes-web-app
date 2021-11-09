bca29f6700d1f22d1e31c6a7a8b155d1927c285c
JRamos29 <malebolge@gmail.com>
2021-11-09 19:14:19 -0300

    Implemented post/recipes endpoint.

--- functions/recipesApi.js
+++ functions/recipesApi.js
@@ -18,7 +18,47 @@ app.use(
 
 app.use(bodyParser.json());
 
-// ~~ RESTFUL CRUD WEB API ENDPOINTS ~~
+// ~~ RESTFUL CRUD WEB API ENDPOINTS ~
+
+app.post('/recipes', async (request, response) => {
+  const authorizationHeader = request.headers['authorization'];
+
+  if (!authorizationHeader) {
+    response.status(401).send('Missing Authorization Header');
+    return;
+  }
+
+  try {
+    await Utilities.authorizeUser(authorizationHeader, auth);
+  } catch (error) {
+    response.status(401).send(error.message);
+    return;
+  }
+
+  const newRecipe = request.body;
+  const missingFields = Utilities.validateRecipePostPut(newRecipe);
+
+  if (missingFields) {
+    response
+      .status(400)
+      .send(`Recipe is not valid. Missing/invalid fields: ${missingFields}`);
+    return;
+  }
+
+  const recipe = Utilities.sanitizeRecipePostPut(newRecipe);
+
+  try {
+    const firestoreResponse = await firestore.collection('recipes').add(recipe);
+
+    const recipeId = firestoreResponse.id;
+
+    response.status(201).send({
+      id: recipeId,
+    });
+  } catch (error) {
+    response.status(400).send(error.message);
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Hello from firebase function express API');
